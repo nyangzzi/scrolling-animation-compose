219e6a48bb63889d290e70e6210d0cf605034548
l2k <ekyi0421@gmail.com>
2024-08-11 02:02:27 +0900

    fix: gradle maven 문법 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.dokka") version "1.7.10"
-}
-
-buildscript {
-    dependencies {
-        classpath ("com.github.dcendents:android-maven-gradle-plugin:2.1")
-    }
+    id("maven-publish") // Add maven-publish plugin
 }
