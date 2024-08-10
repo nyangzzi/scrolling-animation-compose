bbeee5b2d1416a63e31cd3acf3c6faf4aaf1ac84
l2k <ekyi0421@gmail.com>
2024-08-11 02:20:22 +0900

    build: artifacts 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,17 @@ plugins {
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.dokka") version "1.7.10"
     id("maven-publish") // Add maven-publish plugin
+    java
+    `java-library`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven-public") {
+            groupId = "com.msg"
+            artifactId = "library"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
 }
